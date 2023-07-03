8fe0c024e74e01c00ad586d187df333d53753364
Rupam Kundu <rupamkundu1000000@gmail.com>
2023-07-03 15:39:31 +0530

    Added API URL.

--- client/src/types/constants.ts
+++ client/src/types/constants.ts
@@ -1,3 +1,4 @@
-// export const SERVER_ADDRESS = "http://localhost:8080";
 // export const SERVER_ADDRESS = "http://localhost:3333";
-export const SERVER_ADDRESS = "http://localhost:8000";
+// export const SERVER_ADDRESS = "http://localhost:8000";
+// export const SERVER_ADDRESS = "http://localhost:8080";
+export const SERVER_ADDRESS = "https://enterprise-knowledge-retrieval-web.onrender.com";
